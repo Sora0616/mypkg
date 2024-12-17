eea72fdabaf80e257aabbfddaf70425e67feaf76
sora0616 <s23c1115wh@s.chibakoudai.jp>
2024-12-17 11:52:58 +0900

    test

--- mypkg/mypkg/talker.py
+++ mypkg/mypkg/talker.py
@@ -1,17 +1,18 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from person_msgs.msg import Person
 
 rclpy.init()
 node = Node("talker")
-pub = node.create_publisher(Int16, "countup", 10)
+pub = node.create_publisher(Person, "person", 10)
 n = 0 #カウント用変数
 
 
 def cb():
     global n
-    msg = Int16()
-    msg.data = n
+    msg = Person()
+    msg.name = "平野蒼空"
+    msg.age = n
     pub.publish(msg)
     n += 1
 
@@ -19,3 +20,6 @@ def cb():
 def main():
     node.create_timer(0.5, cb)
     rclpy.spin(node)
+
+if __name__ == '__main__': 
+    main()
