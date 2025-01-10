ec527b92e6619bee2623c06901292ca3c90ab876
Sora0616 <s23c1115wh@s.chibakoudai.jp>
2025-01-10 12:09:08 +0900

    new

--- launch/talk_listen.launch.py
+++ launch/talk_listen.launch.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2025 Sora Hirano <s23c1115wh@s.chibakoudai.jp>
+# SPDX-License-Identifier: BSD-3-Clause
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
