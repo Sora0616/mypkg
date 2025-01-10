5a38ca428546ef048f1c7a2e4f17df0ee0824a34
Sora0616 <s23c1115wh@s.chibakoudai.jp>
2025-01-10 12:19:38 +0900

    new

--- setup.py
+++ setup.py
@@ -1,28 +1,33 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'mypkg'
 
 setup(
     name=package_name,
-    version='0.1.0',
+    version='0.0.0',
     packages=[package_name],
-    py_modules=[],
+
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+
     install_requires=['setuptools'],
     zip_safe=True,
+
     maintainer='Sora Hirano',
     maintainer_email='s23c1115wh@s.chibakoudai.jp',
-    description='A ROS 2 package to publish Sotobo Line stations from Chiba to Awa-Kamogawa.',
+    description='a package for practice',
     license='BSD-3-Clause',
+
     tests_require=['pytest'],
+
     entry_points={
         'console_scripts': [
             'station_publisher = mypkg.sotobo_line_stations:main',
         ],
     },
-    data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/talk_listen.launch.py']),
-    ],
 )
 
